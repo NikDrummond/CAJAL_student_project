a96a67fd80b6e795eed3281d2ffdf542432d4d09
Kristopher Torp Jensen <kris@MacBook-Pro.local>
2022-07-25 18:36:42 +0100

    add some comments

--- kris_playground/plot_dims.py
+++ kris_playground/plot_dims.py
@@ -1,20 +1,25 @@
-#%%
+#%% load some libraries and set a few global parameters
 
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 plt.rcParams['font.size'] = 15
 plt.rcParams['axes.spines.right'] = False
 plt.rcParams['axes.spines.top'] = False
 
-#%%
-N = 50 #MB
-Ks = np.arange(2,21) #number of inputs per KC
-S = 14000 #total number of connections
-Ms = np.round(S/Ks, 0).astype(int) #number of KCs
+N = 50 # number of input glomeruli
+Ks = np.arange(2,21) #number of inputs per KC to consider
+S = 14000 #total number of connections (K*M)
+Ms = np.round(S/Ks, 0).astype(int) #number of KCs (S/K)
+f = 0.1 #coding level
+
+#%% compute dimensionality of input current (eq 25, 28)
+
 d1s = 1 / (1/Ms + 1/N*(1+(Ks-1)**2/N)) #no inhibition
 KN2 = Ks**2*(1-Ks/N)**2
 d2s = Ms*KN2/(KN2+(Ms-1)*(Ks**2/N*(1-Ks/N)*(N-Ks)/(N-1))) #inhibition
 
+#plot result
 plt.figure()
 plt.plot(Ks, d1s)
 plt.plot(Ks, d2s)
@@ -22,37 +27,48 @@ plt.xlabel('K')
 plt.ylabel('input current dim')
 plt.show()
 
-#%%
-T = 10000
-f = 0.1
-rep = 3
+#%% compute dimensionality of mixed representation
+T = 10000 #input datapoints to consider
+rep = 3 #number of repetitions (samples from the weight matrix distribution)
 
-dims = np.zeros((len(Ms), 2))
-for iK in range(len(Ks)):
+dims = np.zeros((len(Ms), 2)) #array for storing results
+tic = time.time() #count time
+for iK in range(len(Ks)): #for each K
 
     K, M = Ks[iK], Ms[iK]
 
-    X = np.random.normal(0, 1, (rep, T, N))
-    J = np.zeros((rep, N, M))
-    for r in range(rep):
-        for i in range(M):
+    X = np.random.normal(0, 1, (rep, T, N)) #Gaussian inputs
+    J = np.zeros((rep, N, M)) #weight matrix
+    for r in range(rep): #for each repetition
+        for i in range(M): #for each KC
+            #randomly sample an input weight vector (this could probs be parallelized)
             J[r, np.random.choice(N, K, replace = False), i] = 1
 
-    Ye = X@J
+    Ye = X@J #compute input to KCs
+    #compute input with global feedforward inhibition
     Yi = Ye - K*np.mean(X, axis = -1, keepdims = True)
 
+    #for the cases of no inhibition and inhibition
     for iinh, Y in enumerate([Ye, Yi]):
+        #compute threshold dynamically according to coding level
         thresh = np.quantile(Y, 1-f, axis = 1, keepdims = True)
+        #compute output of nonlinearity
         R = 0.5*np.sign(Y - thresh)+0.5
+        #mean subtract to compute covariance
         R = R - np.mean(R, axis = 1, keepdims = True)
+        #compute covariance matrix
         C = R.transpose(0,2,1) @ R
+        #compute eigenvalues
         evals = np.linalg.eigvalsh(C)
+        #compute dimensionality (participation ratio)
         dim = np.sum(evals, axis = -1)**2 / np.sum(evals**2, axis = -1)
+        #store result
         dims[iK, iinh] = np.mean(dim)
 
-    print(K, dims[iK, :]/N)
+    #print progress
+    print(K, dims[iK, :]/N, np.round(time.time() - tic, 3))
 
-# %%
+# %% plot result
 
 print(Ks[np.argmax(dims, axis = 0)])
 print(np.amax(dims, axis = 0)/N)
